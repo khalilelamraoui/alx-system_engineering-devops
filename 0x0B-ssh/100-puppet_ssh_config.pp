c42dd7964f347b297d1aa64accd4d18455b7a3f6
khalilelamraoui <khalilelam16@gmail.com>
2024-03-23 22:33:26 +0000

    fixed problem

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Connecting to a server without a password
 
-file_line {
+file_line { 'No PasswordAuthentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line {
+file_line { 'File':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
