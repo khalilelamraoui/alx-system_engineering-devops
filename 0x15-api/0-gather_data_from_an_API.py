8d70cb11aff32a45fd6ce4d787c4a326ed8597a9
khalilelamraoui <khalilelam16@gmail.com>
2024-04-22 23:01:40 +0100

    fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,36 @@
 #!/usr/bin/python3
+
 """
-Script that, using a REST API,
-for a given employee ID, returns information about his/her TODO list progress.
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
-import requests
-import sys
-
-
-def get_employee_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    employee_url = (f'{base_url}/users/{employee_id}')
-    todos_url = (f'{base_url}/todos?userId={employee_id}')
-
-    # Fetching employee information
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data['name']
-
-    # Fetching TODO list
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculating progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-    # Displaying progress
-    print(f"Employee {employee_name} is done with tasks ("
-          f"{completed_tasks}/{total_tasks}):")
-    for todo in todos_data:
-        if todo['completed']:
-            print(f"\t{todo['title']}")
+from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    r1 = get('https://jsonplaceholder.typicode.com/todos/')
+    data = r1.json()
+    done_tasks = 0
+    all_tasks = 0
+    tasks = []
+    r1 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = r1.json()
+
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            user = i.get('name')
+
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            all_tasks += 1
+
+            if i.get('completed') is True:
+                done_tasks += 1
+                tasks.append(i.get('title'))
+
+    print(f"Employee {user} is done with tasks({done_tasks}/{all_tasks}):")
+
+    for i in tasks:
+        print("\t {}".format(i))
